82893165127f63b411910132758c249e7cf449b9
Himanshu-Gautam20 <himanshugautam9696@gmail.com>
2025-08-13 11:27:28 +0530

    updated the cors issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,4 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
 });
