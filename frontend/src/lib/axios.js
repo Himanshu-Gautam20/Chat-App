ffeeaa7fdf790e3bb425fba6a507b722cc9faf06
Himanshu-Gautam20 <himanshugautam9696@gmail.com>
2025-08-13 11:02:02 +0530

    removed console.log

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-console.log(import.meta.env.VITE_BACKEND_URL);
-
 export const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
   withCredentials: true,
